fe084a19c2513d52d6c9a629d43bcff6b047ac3f
henryhtm <henryhtm@gmail.com>
2018-11-15 09:22:24 +0800

    Update cmd.sh

--- hbase-single/file/cmd.sh
+++ hbase-single/file/cmd.sh
@@ -2,7 +2,12 @@
 
 trap "echo 'stop hbase';/usr/local/hbase/bin/stop-hbase.sh;sleep 5" TERM
 
+read HostName < /etc/hostname
+echo "127.0.0.1 $HostName"> /etc/hosts
+
 /usr/local/hbase/bin/start-hbase.sh
+sleep 10
+hbase-daemon.sh start thrift
 
 while true ; do
 sleep 2
